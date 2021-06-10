a6dccffd9af8791a6441375542093fc2f4abb69b
Menji <jennifer.menjivar18@gmail.com>
2021-06-10 09:51:27 -0400

    couldn't add btn but adjusted size of photo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
-var movieTitleEl = document.querySelector('#movie-title');
+var movieTitleEl = document.querySelector("#movie-title");
+var recipeUrl =
+  "https://api.spoonacular.com/recipes/random?apiKey=f999bb7189a34392a1a531b4ec949828";
 
+// to initialize the select dropdown menu for movie (needed for materialize)
 $(document).ready(function () {
   $("select").formSelect();
 });
 
-var recipeUrl =
-  "https://api.spoonacular.com/recipes/random?apiKey=f999bb7189a34392a1a531b4ec949828";
+// to initialize the fav btns (needed for materialize)
+$(document).ready(function () {
+  $(".fixed-action-btn").floatingActionButton();
+});
 
+// to generate a random movie genre
 var getMovieGenres = function () {
   var apiUrl =
     "https://api.themoviedb.org/3/genre/movie/list?api_key=4a2daec3e9790c72eaaf5273d699af37&language=en-US";
@@ -18,23 +24,23 @@ var getMovieGenres = function () {
       }
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
       var randomGenre =
         data.genres[Math.floor(Math.random() * data.genres.length)];
-      console.log(randomGenre.name);
+      // console.log(randomGenre.name);
     });
 };
 
-// to retrieve an array of the selected genres from the user
+// to create an array from the user's selected genre(s)
 $("#movie-form").submit(function (event) {
   var selectedGenres = $("#genres-form").val();
   movieByGenre(selectedGenres);
   event.preventDefault();
 });
 
-// function to get movie by user selected genre
+// function to get a random movie from the user's selected genre(s)
 var movieByGenre = function (genreIds) {
-  // to make selected genre array into a string
+  // to make selectedGenres array into a string in order to add to url
   var genresString = genreIds.toString();
   // console.log(genresString);
 
@@ -48,28 +54,29 @@ var movieByGenre = function (genreIds) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      // console.log(data);
       // to retrieve a random movie from the selected genre(s)
       var randomMovie =
         data.results[Math.floor(Math.random() * data.results.length)];
-      
-      displayMovieTitle(randomMovie.title)
-      displayMovieImage(randomMovie.poster_path)
+
+      displayMovieTitle(randomMovie.title);
+      displayMovieImage(randomMovie.poster_path);
     });
 };
 
+// append movie title to page
 function displayMovieTitle(title) {
-
   $("#movie-title").html("");
   var movieTitle = $("<p>").text(title);
   $("#movie-title").append(movieTitle);
 }
 
+// append movie img to page
 function displayMovieImage(img) {
   $("#movie-poster").html("");
-  var moviePosterUrl = "https://image.tmdb.org/t/p/original/" + img
-  console.log(moviePosterUrl)
-  var movieTag = $("<img>").attr("src", moviePosterUrl)
+  var moviePosterUrl = "https://image.tmdb.org/t/p/original/" + img;
+  console.log(moviePosterUrl);
+  var movieTag = $("<img>").attr("src", moviePosterUrl);
   $("#movie-poster").append(movieTag);
 }
 
@@ -103,7 +110,7 @@ function getRecipeImg(num) {
   $("#food-pic").append(imgTag);
 }
 
-function displayRecipeName (name) {
+function displayRecipeName(name) {
   $("#recipe-name").html("");
   var recipeName = $("<p>").text(name);
   $("#recipe-name").append(recipeName);
